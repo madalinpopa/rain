7c7731bc9f6790f390946114a07057186e8b94f4
Madalin Popa <contact@madalinpopa.com>
2024-05-23 23:07:09 +0300

    chore: Update weather API request and JSON handling in main.go

--- weather/weather.go
+++ weather/weather.go
@@ -16,8 +16,22 @@ type City struct {
 	Country string  `json:"country"`
 }
 
-type Weather struct {
-	Temp float64 `json:"temp"`
+type Forecasts struct {
+	Current struct {
+		Temp    float64 `json:"temp"`
+		Weather []struct {
+			Description string `json:"description"`
+		} `json:"weather"`
+	} `json:"current"`
+	Hourly []struct {
+		Dt        int64   `json:"dt"`
+		Temp      float64 `json:"temp"`
+		FeelsLike float64 `json:"feels_like"`
+		Weather   []struct {
+			Description string `json:"description"`
+		} `json:"weather"`
+		Pop float64 `json:"pop"`
+	} `json:"hourly"`
 }
 
 func GetApiEnv(envName string) (string, error) {
@@ -50,6 +64,14 @@ func unmarshalCities(body []byte) ([]City, error) {
 	return cities, nil
 }
 
+func unmarshalForecasts(body []byte) (Forecasts, error) {
+	var forecasts Forecasts
+	if err := json.Unmarshal(body, &forecasts); err != nil {
+		return Forecasts{}, err
+	}
+	return forecasts, nil
+}
+
 func FetchCityData(apiKey, location string) ([]City, error) {
 
 	baseUrl := "https://api.openweathermap.org/geo/1.0/direct?"
@@ -72,17 +94,20 @@ func FetchCityData(apiKey, location string) ([]City, error) {
 
 }
 
-func FetchWeatherData(apiKey, lat, lon string) (Weather, error) {
+func FetchWeatherData(apiKey, lat, lon string) (Forecasts, error) {
 
 	baseUrl := "https://api.openweathermap.org/data/3.0/onecall?"
-	url := fmt.Sprintf("%slat=%s&lon=%s&appid=%s", baseUrl, lat, lon, apiKey)
+	url := fmt.Sprintf("%slat=%s&lon=%s&units=metric&lang=ro&appid=%s", baseUrl, lat, lon, apiKey)
 
 	respBody, err := makeGetRequest(url)
 	if err != nil {
-		return Weather{}, fmt.Errorf("failed to make GET request: %w", err)
+		return Forecasts{}, fmt.Errorf("failed to make GET request: %w", err)
 	}
 
-	fmt.Println(respBody)
+	forecasts, err := unmarshalForecasts(respBody)
+	if err != nil {
+		return Forecasts{}, fmt.Errorf("failed to unmarshal JSON: %w", err)
+	}
 
-	return Weather{}, nil
+	return forecasts, nil
 }
