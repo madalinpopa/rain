26805940fb9b8b3e98e0edfcf8974c08a2fa8d92
Madalin Popa <contact@madalinpopa.com>
2024-05-23 21:35:44 +0300

    chore: Refactor weather API request and JSON handling in main.go

--- main.go
+++ main.go
@@ -17,6 +17,10 @@ type City struct {
 	Country string  `json:"country"`
 }
 
+type Weather struct {
+	Temp float64 `json:"temp"`
+}
+
 func getApiEnv(envName string) (string, error) {
 	api := os.Getenv(envName)
 	if api == "" {
@@ -47,6 +51,21 @@ func fetchCityData(apiKey, location string) ([]City, error) {
 
 }
 
+func fetchWeatherData(apiKey, lat, lon string) ([]byte, error) {
+
+	baseUrl := "https://api.openweathermap.org/data/3.0/onecall?"
+	url := fmt.Sprintf("%slat=%s&lon=%s&appid=%s", baseUrl, lat, lon, apiKey)
+
+	respBody, err := makeGetRequest(url)
+	if err != nil {
+		return respBody, fmt.Errorf("failed to make GET request: %w", err)
+	}
+
+	fmt.Println(respBody)
+
+	return respBody, nil
+}
+
 func makeGetRequest(url string) ([]byte, error) {
 	resp, err := http.Get(url)
 	if err != nil {
@@ -70,12 +89,7 @@ func unmarshalCities(body []byte) ([]City, error) {
 }
 
 // getLocation retrieves the location data
-func getLocation(location string) (City, error) {
-	apiKey, err := getApiEnv("OPEN_WEATHER_API_KEY")
-	if err != nil {
-		return City{}, err
-	}
-
+func getLocation(location, apiKey string) (City, error) {
 	cities, err := fetchCityData(apiKey, location)
 	if err != nil {
 		return City{}, err
@@ -84,13 +98,34 @@ func getLocation(location string) (City, error) {
 	return cities[0], nil
 }
 
-func main() {
+// getWeather retrieves the weather data
+func getWeather(city City, apiKey string) (Weather, error) {
+	weather, err := fetchWeatherData(apiKey, fmt.Sprintf("%f", city.Lat), fmt.Sprintf("%f", city.Lon))
+	if err != nil {
+		return Weather{}, err
+	}
 
+	fmt.Println(weather)
+
+	return Weather{}, nil
+}
+
+func main() {
 	defaultLocation := "Bucharest"
-	city, err := getLocation(defaultLocation)
+
+	apiKey, err := getApiEnv("OPEN_WEATHER_API_KEY")
+	if err != nil {
+		log.Fatalf("Error getting API key: %v", err)
+	}
+
+	city, err := getLocation(defaultLocation, apiKey)
 	if err != nil {
 		log.Fatalf("Error getting location: %v", err)
 	}
+
+	weather, err := getWeather(city, apiKey)
+
+	fmt.Println(weather, err)
 
 	fmt.Printf("City: %s, Lat: %f, Lon: %f, Country: %s\n", city.Name, city.Lat, city.Lon, city.Country)
 
